b8c23393809222922e1a9b442f9489b2a29916d6
julian <julian@cypherstack.com>
2025-02-13 11:49:35 -0600

    update tests with values to be consistent with electrum-nmc

--- test/script_names_test.dart
+++ test/script_names_test.dart
@@ -1,11 +1,11 @@
-import 'dart:convert';
-
+import 'package:coinlib/coinlib.dart';
 import 'package:namecoin/namecoin.dart';
 import 'package:test/test.dart';
 
-const name = 'd/testsw';
-const privKey = 'TjjiQXjN7H6caZi2R3qEKNtgvvdVB2ZZGgKg1EpETWZiVHmg7xL2';
-const expectedSalt = "119437b0c7e3c1a225e3d50cd7357e3f61682647";
+const name = 'mytestname';
+const privKeyHex =
+    'bdf038bbab2e95bb184b5327dec4c0ced5adfe100745f5ddcc17f6da8f8d876f';
+const expectedSalt = "0539dedcdb7a0f428d14cdcc27f217e0a01ad9f6";
 
 void main() {
   /// Tests to check if the 3 pubScriptKey constructions of the name operations execute without throwing exception.
@@ -17,14 +17,14 @@ void main() {
 
   /// Test that using the same private key with the same name will produce the same salt.
   test("Deterministic salt", () {
-    final (_, salt, _) = scriptNameNew(name, ascii.encode(privKey));
-    final (_, saltIdentical, _) = scriptNameNew(name, ascii.encode(privKey));
+    final (_, salt, _) = scriptNameNew(name, hexToBytes(privKeyHex));
+    final (_, saltIdentical, _) = scriptNameNew(name, hexToBytes(privKeyHex));
     expect(salt, saltIdentical);
   });
 
   /// Verifies that scriptNameNew generates the correct salt for a given name and private key.
   test("scriptNameNew generates expected salt", () {
-    final (_, salt, _) = scriptNameNew(name, ascii.encode(privKey));
+    final (_, salt, _) = scriptNameNew("mytestname", hexToBytes(privKeyHex));
     expect(salt.toString(), expectedSalt);
   });
 }

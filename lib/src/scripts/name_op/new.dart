61cb231b437371cfb86506b54a84ff5022afe1da
julian <julian@cypherstack.com>
2025-02-13 09:41:18 -0600

    update deterministic salt gen to use existing dependencies

--- lib/src/scripts/name_op/new.dart
+++ lib/src/scripts/name_op/new.dart
@@ -2,11 +2,13 @@ import 'dart:convert';
 import 'dart:typed_data';
 
 import 'package:coinlib/coinlib.dart';
-import 'package:cryptography/cryptography.dart';
 import 'package:namecoin/src/constants.dart';
 import 'package:namecoin/src/scripts/tools.dart';
 import 'package:namecoin/src/scripts/validators.dart';
 import 'package:pointycastle/api.dart';
+import 'package:pointycastle/digests/sha256.dart';
+import 'package:pointycastle/key_derivators/api.dart';
+import 'package:pointycastle/key_derivators/hkdf.dart';
 
 /// Generate a pubScriptKey to register a new name.
 ///
@@ -15,21 +17,20 @@ import 'package:pointycastle/api.dart';
 /// when first updating the name from this output.
 /// Returns (script, salt, commitment)
 /// This pubscriptKey can be inserted into an output for a new transaction.
-Future<(String, String, String)> scriptNameNew(String name,
-    [Uint8List? privkey]) async {
+(String, String, String) scriptNameNew(String name, [Uint8List? privkey]) {
   final identifier = ascii.encode(name);
   validateIdentifierLength(identifier);
-  final (nameOp, salt) = await _buildNameNew(identifier, privkey);
+  final (nameOp, salt) = _buildNameNew(identifier, privkey);
   final saltHex = bytesToHex(salt);
   final commitment = bytesToHex(nameOp["commitment"] as Uint8List);
   return (nameOpToScript(nameOp), saltHex, commitment);
 }
 
-Future<(Map<String, Object>, Uint8List)> _buildNameNew(Uint8List identifier,
-    [Uint8List? privkey]) async {
+(Map<String, Object>, Uint8List) _buildNameNew(Uint8List identifier,
+    [Uint8List? privkey]) {
   final Uint8List salt;
   if (privkey != null) {
-    salt = await saltFromPrivKey(privkey, identifier);
+    salt = saltFromPrivKey(privkey, identifier);
   } else {
     salt = generateRandomBytes(20);
   }
@@ -41,17 +42,11 @@ Future<(Map<String, Object>, Uint8List)> _buildNameNew(Uint8List identifier,
 /// Create a salt for a NAME_NEW operation using the private key from the address on which the name op will be sent.
 ///
 /// It allows to retrieve the salt deterministically from the mnemonic/address/name.
-Future<Uint8List> saltFromPrivKey(
-  Uint8List privkey,
-  Uint8List identifier,
-) async {
-  final hkdf = Hkdf(
-    hmac: Hmac.sha256(),
-    outputLength: 32,
-  );
-  final secretKey = SecretKey(privkey);
-  final info = ascii.encode('Namecoin Registration Salt');
-  final salt =
-      await hkdf.deriveKey(secretKey: secretKey, nonce: identifier, info: info);
-  return Uint8List.fromList(salt.bytes.sublist(0, 20));
+Uint8List saltFromPrivKey(Uint8List privkey, Uint8List identifier) {
+  final hkdf = HKDFKeyDerivator(SHA256Digest());
+  final info = Uint8List.fromList(ascii.encode('Namecoin Registration Salt'));
+  hkdf.init(HkdfParameters(privkey, 32, identifier, info));
+  final derivedKey = Uint8List(32);
+  hkdf.deriveKey(null, 0, derivedKey, 0);
+  return derivedKey.sublist(0, 20);
 }
--- test/script_names_test.dart
+++ test/script_names_test.dart
@@ -5,20 +5,26 @@ import 'package:test/test.dart';
 
 const name = 'd/testsw';
 const privKey = 'TjjiQXjN7H6caZi2R3qEKNtgvvdVB2ZZGgKg1EpETWZiVHmg7xL2';
+const expectedSalt = "119437b0c7e3c1a225e3d50cd7357e3f61682647";
 
 void main() {
   /// Tests to check if the 3 pubScriptKey constructions of the name operations execute without throwing exception.
-  test("NameNew output", () async {
-    final (_, salt, _) = await scriptNameNew(name);
+  test("NameNew output", () {
+    final (_, salt, _) = scriptNameNew(name);
     scriptNameFirstUpdate(name, "myvalue", salt);
     scriptNameUpdate(name, "mysecondvalue");
   });
 
   /// Test that using the same private key with the same name will produce the same salt.
-  test("Deterministic salt", () async {
-    final (_, salt, _) = await scriptNameNew(name, ascii.encode(privKey));
-    final (_, saltIdentical, _) =
-        await scriptNameNew(name, ascii.encode(privKey));
+  test("Deterministic salt", () {
+    final (_, salt, _) = scriptNameNew(name, ascii.encode(privKey));
+    final (_, saltIdentical, _) = scriptNameNew(name, ascii.encode(privKey));
     expect(salt, saltIdentical);
+  });
+
+  /// Verifies that scriptNameNew generates the correct salt for a given name and private key.
+  test("scriptNameNew generates expected salt", () {
+    final (_, salt, _) = scriptNameNew(name, ascii.encode(privKey));
+    expect(salt.toString(), expectedSalt);
   });
 }

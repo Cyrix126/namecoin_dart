5906242a036fcfb7ca314adb79b5b1edf36ecadd
Cyrix126 <58007246+Cyrix126@users.noreply.github.com>
2025-02-13 12:06:10 +0100

    feat: allows to derive deterministic salt from private key

--- lib/src/scripts/name_op/new.dart
+++ lib/src/scripts/name_op/new.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 import 'dart:typed_data';
 
 import 'package:coinlib/coinlib.dart';
+import 'package:cryptography/cryptography.dart';
 import 'package:namecoin/src/constants.dart';
 import 'package:namecoin/src/scripts/tools.dart';
 import 'package:namecoin/src/scripts/validators.dart';
@@ -14,18 +15,43 @@ import 'package:pointycastle/api.dart';
 /// when first updating the name from this output.
 /// Returns (script, salt, commitment)
 /// This pubscriptKey can be inserted into an output for a new transaction.
-(String, String, String) scriptNameNew(String name) {
+Future<(String, String, String)> scriptNameNew(String name,
+    [Uint8List? privkey]) async {
   final identifier = ascii.encode(name);
   validateIdentifierLength(identifier);
-  final (nameOp, salt) = _buildNameNew(identifier);
+  final (nameOp, salt) = await _buildNameNew(identifier, privkey);
   final saltHex = bytesToHex(salt);
   final commitment = bytesToHex(nameOp["commitment"] as Uint8List);
   return (nameOpToScript(nameOp), saltHex, commitment);
 }
 
-(Map<String, Object>, Uint8List) _buildNameNew(Uint8List identifier) {
-  final salt = generateRandomBytes(20);
+Future<(Map<String, Object>, Uint8List)> _buildNameNew(Uint8List identifier,
+    [Uint8List? privkey]) async {
+  final Uint8List salt;
+  if (privkey != null) {
+    salt = await saltFromPrivKey(privkey, identifier);
+  } else {
+    salt = generateRandomBytes(20);
+  }
   final commitment = Digest("RIPEMD-160")
       .process(sha256Hash(Uint8List.fromList(salt + identifier)));
   return ({"op": opCodeNameNewValue, "commitment": commitment}, salt);
+}
+
+/// Create a salt for a NAME_NEW operation using the private key from the address on which the name op will be sent.
+///
+/// It allows to retrieve the salt deterministically from the mnemonic/address/name.
+Future<Uint8List> saltFromPrivKey(
+  Uint8List privkey,
+  Uint8List identifier,
+) async {
+  final hkdf = Hkdf(
+    hmac: Hmac.sha256(),
+    outputLength: 32,
+  );
+  final secretKey = SecretKey(privkey);
+  final info = ascii.encode('Namecoin Registration Salt');
+  final salt =
+      await hkdf.deriveKey(secretKey: secretKey, nonce: identifier, info: info);
+  return Uint8List.fromList(salt.bytes.sublist(0, 20));
 }
--- test/script_names_test.dart
+++ test/script_names_test.dart
@@ -1,12 +1,24 @@
+import 'dart:convert';
+
 import 'package:namecoin/namecoin.dart';
 import 'package:test/test.dart';
 
 const name = 'd/testsw';
+const privKey = 'TjjiQXjN7H6caZi2R3qEKNtgvvdVB2ZZGgKg1EpETWZiVHmg7xL2';
+
 void main() {
   /// Tests to check if the 3 pubScriptKey constructions of the name operations execute without throwing exception.
-  test("NameNew output", () {
-    final (_, salt, _) = scriptNameNew(name);
+  test("NameNew output", () async {
+    final (_, salt, _) = await scriptNameNew(name);
     scriptNameFirstUpdate(name, "myvalue", salt);
     scriptNameUpdate(name, "mysecondvalue");
+  });
+
+  /// Test that using the same private key with the same name will produce the same salt.
+  test("Deterministic salt", () async {
+    final (_, salt, _) = await scriptNameNew(name, ascii.encode(privKey));
+    final (_, saltIdentical, _) =
+        await scriptNameNew(name, ascii.encode(privKey));
+    expect(salt, saltIdentical);
   });
 }
